b9749c5a27d0dbe68e3f7c2d08b44055983caa1f
colorodriguezf <colitorodriguez97@gmail.com>
2023-11-16 17:55:45 -0300

    agrego js con arreglos con palabras de 4,5 y 6 letras ya que las apis que busque o no funcionaban, o daban porcas peticiones por dia gratis. Falta agregar de 7 palabras

---
+++ js/palabrasRandom.js
@@ -0,0 +1,52 @@
+"use strict";
+
+
+const palabras4Letras = [
+    "lago", "boca", "nuez", "pino", "pila", "hoja", "pies", "bien", "pato", "jugo",
+    "olla", "pala", "roca", "loma", "ceja", "dedo", "vino", "alma", "seda", "miga",
+    "fama", "rosa", "mota", "cien", "mono", "cima", "lila", "tela", "luna", "trap",
+    "sera", "rama", "rojo", "real", "hijo", "cedo", "mudo", "puño", "casa", "paso",
+    "pera", "gato", "fino", "mesa", "mira", "arco", "foto", "puma", "foca", "palo",
+    "paja", "mina", "taza", "cola", "cama", "roto", "coco", "lata", "cubo", "auto",
+    "tasa", "arte", "peña", "rayo", "juez", "pelo", "mano", "teja", "lana", "jefe",
+    "cuna", "rato", "lado", "hilo", "foco", "cebo", "pico", "lima", "arpa", "dama", 
+    "dado", "lona", "saco", "raya", "poro", "lupa", "tren", "trio", "rana", "peso",
+    "pata", "hola", "sala", "piso", "saca", "cuba", "pozo", "mata", "cara", "cana",
+    "iban", "abre", "amas", "amen", "apto", "arde", "alas", "aves", "bota", "vota",
+    "cabe", "ceno", "ceso", "ceco", "cojo", "creo", "dias", "daño", "dejo", "doma",
+    "duos", "echo", "ecos", "flor", "hace", "haga", "mear", "caca", "mete", "cera",
+    "mate", "leon", "vaca", "toro", "modo", "moda", "miel", "risa", "demo", "seco",
+    "neon", "amar", "leer"
+  ];
+
+  const palabras5Letras = [
+    "cable", "arena", "barco", "ceibo", "plata", "danza", "llama", "trato", "tenis", "hacer",
+    "flora", "huevo", "lejos", "trozo", "risas", "tango", "ramal", "papel", "crudo", "canto",
+    "ganar", "salto", "gallo", "gloria", "girar","ronda", "deber", "cosas", "mesas", "lugar",
+    "comer", "logro", "beber", "gente", "herir", "reses", "farol", "junta", "local", "hongo",
+    "mismo", "monte", "grado", "plato", "quema", "quien", "radar", "salir", "sobre", "torre",
+    "tenor", "trigo", "valle", "volar", "cruel", "culto", "abrir", "acero", "feliz", "fuego",
+    "funda", "golpe", "calle", "calor", "coche", "coraz", "cuota", "darlo", "decir", "dedos",
+    "ocaso", "globo", "circo", "risco", "largo", "debil", "plena", "tigre", "villa", "quedo",
+    "reloj", "linea", "aroma", "lince", "tempo", "grano", "pilar", "robot", "cieno", "medio",
+    "miedo", "libro", "nieve", "coral", "sello", "hojal", "fusil", "marzo", "venus", "gripe",
+    "lecho", "dorso", "tonel", "rifle", "genio", "lomos", "usado", "vapor", "bruma", "gorro",
+    "tumba", "voces", "fusor", "bicho", "techo", "nasal", "muros", "domar", "fiera", "cedro",
+    "penas", "rutas", "tener", "corre", "campo", "nadar", "jugar", "mirar", "vivir", "carro",
+    "joven", "fugaz", "rumor", "total", "valor", "yogur", "justo", "cerro", "lucha", "surco",
+    "migas", "vuelo", "placa", "droga", "regir", "lento", "breve", "perla"
+];
+
+const palabras6Letras = [
+    "cadena", "corona", "granos", "piloto", "castor", "gritar", "manada", "camino", "cartel", "delfin",
+    "jabali", "sombra", "guerra", "cuerda", "rutina", "pueblo", "bocina", "piedra", "rodaja", "cruzar",
+    "halito", "jabali", "suceda", "cadete", "gaveta", "inmune", "indigo", "indico", "girado", "brocha",
+    "cuello", "amable", "sabido", "depues", "duelos", "calada", "cagada", "robote", "finado", "molina",
+    "quedar", "solida", "salida", "marina", "doblar", "manojo", "rondar", "bordar", "metodo", "siluro",
+    "guitar", "racimo", "grande", "madero", "mesada", "procer", "moreno", "ceniza", "roedor", "saciar",
+    "termos", "brutal", "colina", "quedar", "luchar", "cuento", "rufian", "sabana", "harina", "casero",
+    "drogas", "brazos", "viendo", "cercos", "locura", "jugada", "aplico", "gacela", "sobres", "libero",
+    "farola", "senado", "muelle", "beicon", "lograr", "fuente", "helado", "ciudad", "gracia", "perlas"
+  ];
+  
+
